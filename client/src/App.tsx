b37ed1d2ad0ffbdfaf2e5bf97241fb50b0526691
ssannejohansson <sannej0hansson@hotmail.com>
2026-05-13 09:07:09 +0200

    Add oauth callback handler and login button redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,30 @@
+import { Routes, Route } from "react-router-dom";
 import { WindowFrame } from "./components/wmp/WindowFrame";
 import { Login } from "./pages/Login";
+import { AuthCallback } from "./pages/AuthCallback.js";
 import "./App.css"
 
-function App() {
+export default function App() {
     return (
-        <WindowFrame title="Windows Media Player">
-            <Login />
-        </WindowFrame>
-    )
+      <WindowFrame title="Windows Media Player">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/auth/callback" element={<AuthCallback />} />
+          <Route
+            path="/library"
+            element={<div className="text-wmp-blue">Library (coming soon)</div>}
+          />
+          <Route
+            path="/search"
+            element={<div className="text-wmp-blue">Search (coming soon)</div>}
+          />
+          <Route
+            path="/now-playing"
+            element={
+              <div className="text-wmp-blue">Now Playing (coming soon)</div>
+            }
+          />
+        </Routes>
+      </WindowFrame>
+    );
 }
-
-export default App
