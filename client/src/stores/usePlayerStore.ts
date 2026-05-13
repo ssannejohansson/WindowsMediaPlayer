9ce939e0a899deee7843c4d662d2e4c5414841aa
ssannejohansson <sannej0hansson@hotmail.com>
2026-05-13 10:18:26 +0200

    Add usePlayerStore and spotify web playback SDK initializer

---
+++ client/src/stores/usePlayerStore.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+import type { Track } from '../types/spotify.js'
+
+type PlayerState = {
+  deviceId: string | null
+  isPlaying: boolean
+  currentTrack: Track | null
+  progressMs: number | null
+  setPlaybackState: (payload: Partial<Omit<PlayerState, 'setPlaybackState'>>) => void
+  clearPlayback: () => void
+}
+
+export const usePlayerStore = create<PlayerState>((set) => ({
+  deviceId: null,
+  isPlaying: false,
+  currentTrack: null,
+  progressMs: null,
+  setPlaybackState: (payload) => set((state) => ({ ...state, ...payload })),
+  clearPlayback: () => set({ deviceId: null, isPlaying: false, currentTrack: null, progressMs: null }),
+}))
